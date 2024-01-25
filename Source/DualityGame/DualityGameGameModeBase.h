81b0d98664a587f8bb725744f8939f4fd3f16a65
anmavtb <vincent.guiraudou@gmail.com>
2024-01-25 10:55:44 +0100

    Mise en place de Unreal

---
+++ Source/DualityGame/DualityGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DualityGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUALITYGAME_API ADualityGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
